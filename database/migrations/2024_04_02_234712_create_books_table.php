163d7d95313dbed7b5874c28514045ba550c32c9
sanaa <sanaaennaji93@gmail.com>
2024-04-03 02:39:14 +0000

    migration

---
+++ database/migrations/2024_04_02_234712_create_books_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title' , 200);
+            $table->string('image')->nullable();
+            $table->string('author')->nullable();
+            $table->integer('ISBN')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->string('status' ,45)->nullable();
+            $table->longText('description')->nullable();
+            $table->decimal('price');
+            $table->foreignId('store_id')
+            ->constrained('stores')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->foreignId('categorie_id')
+            ->constrained('categories')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->string('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
