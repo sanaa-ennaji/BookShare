ce3a57c87eb39c09186a3f5d3025467b04a9eb8e
sanaa <sanaaennaji93@gmail.com>
2024-04-05 11:47:46 +0000

    add repository pattern

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -12,64 +12,7 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     
-    public function Register (Request $request){
-       
-        $datad = $request->validate([
-            'name' => ['required'],
-            'email' => ['required'],
-            'password'=>['required'],
-            'address'=>['required'],
-        ]);
-       
-        $data = $request->validate([
-            'phone'=>['required'],
-         ]);
-        $datad['password'] = bcrypt($datad['password']);
-       $user= User::create($datad);
-       auth()->login($user);
-       $costumer = Costumer::create([
-        'user_id' => $user->id,
-        'phone' => $data 
-    ]);
-
-          return redirect('/');
-      
-    }
-
-    public function logout (){
-        auth()->logout();
-        return redirect('/') ;
-    }
-
-    public function login(Request $request)
-    {
-        $data = $request->validate([
-            'logemail' => 'required',
-            'logpassword' => 'required'
-        ]);
-    
-        if (auth()->attempt(['email' => $data['logemail'], 'password' => $data['logpassword']])) {
-            $request->session()->regenerate();
-    
-            $user = auth()->user();
-    
-            if ($user->status == 0) {
-               
-                abort(404, 'User not found');
-            }
-    
-            if ($user->role === 'admin') {
-                return redirect('/admin');
-            } elseif ($user->role === 'store') {
-                return redirect('/store');
-            } elseif ($user->role === 'client') {
-                return redirect('/client');
-            }
-        }
-    
-        return redirect()->back()->with('error', 'Invalid credentials');
-    }
-    
+   
     
 
     public function showUsers(){
---
+++ app/RepositoryInterfaces/AdminRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\RepositoryInterfaces;
+
+interface UsersRepositoryInterface
+{
+    public function CostumerRegister(array $data);
+    public function findUserById($id);
+    public function updateUser($id, array $data);
+    public function deleteUser($id);
+    public function getAllUsers();
+    public function getArchivedUsers();
+}
