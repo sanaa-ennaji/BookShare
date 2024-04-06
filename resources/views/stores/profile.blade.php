dc2d5a89254f15725211f2c5b965a05aa4b66b69
sanaa <sanaaennaji93@gmail.com>
2024-04-06 21:50:18 +0000

    add image

--- resources/views/stores/profile.blade.php
+++ resources/views/stores/profile.blade.php
@@ -10,17 +10,15 @@
     <body >
       @include('components/navbar')
         @include('components/navbar2')
-        <div class="border-1 shadow-lg shadow-gray-700 rounded-lg sm:h-[20vh] sm:w-[80%] mx-auto" style="background-image: url(../img/Thunder.png)">
+        <div class="border-1 shadow-lg shadow-gray-700 sm:h-[45vh] sm:w-[80%] mx-auto" style="background-image: url(..img/pexels-tuur-tisseghem-626986.jpg)">
             <div class="flex rounded-t-lg bg-top-color sm:px-2 w-full">
-                <div class="h-40 w-40 overflow-hidden rounded-full sm:relative sm:p-0 sm:top-20 top-40 left-5 p-3">
-                    <img src="../img/bookstore.jpg"/>
+                <div class="overflow-hidden rounded-full sm:relative sm:p-0 sm:top-60 top-35 h-32 w-32 sm:w-25 sm:h-25 ">
+                    <img class="object-cover object-center h-32 sm:h-25" src="../img/bookstore.jpg"/>
                 </div>
-    
                 <div class="w-2/3 sm:text-center pl-5 mt-10 text-start">
                     <p class="font-poppins font-bold text-heading sm:text-4xl text-2xl">
                         Amit Pachange
                     </p>
                     <p class="text-heading">Software Engineer</p>
                 </div>
-    
             </div>
