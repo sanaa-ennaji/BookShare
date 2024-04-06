c6528fbc09c5b7aa3f0e8425a2139d7cdfaff0e4
sanaa <sanaaennaji93@gmail.com>
2024-04-06 23:07:33 +0000

    add store register inputs

--- resources/views/stores/StoreRegister.blade.php
+++ resources/views/stores/StoreRegister.blade.php
@@ -12,4 +12,142 @@
         @include('components/navbar')
 
         @include('components/navbar2')
+
+
+        <div class="max-w-screen-lg sm:w-[80%] mx-auto p-5">
+            <div class="grid grid-cols-1 md:grid-cols-12 border">
+                <div class="bg-red-500 md:col-span-4 p-10 text-white">
+                    <p class="mt-4 text-sm leading-7 font-regular uppercase">
+                        Contact
+                    </p>
+                    <h3 class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight">
+                        join<span class="text-red-900"> Us</span>
+                    </h3>
+                    <p class="mt-4 leading-7 text-gray-200">
+                        your fisrt step to make your Business  online 
+                        fill the form and wait for acception we will send you an email .
+                    </p>
         
+                    <div class="flex items-center mt-5">
+                        <svg class="h-6 mr-2 text-indigo-600" fill="currentColor" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 489.536 489.536"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 489.536 489.536">
+                            <g>
+                                <g>
+                                    <path
+                                        d="m488.554,476l-99-280.2c-1-4.2-5.2-7.3-9.4-7.3h-45.6c12.9-31.1 19.6-54.9 19.6-70.8 0-64.6-50-117.7-112.5-117.7-61.5,0-112.5,52.1-112.5,117.7 0,17.6 8.2,43.1 19.9,70.8h-39.7c-4.2,0-8.3,3.1-9.4,7.3l-99,280.2c-3.2,10.3 6.3,13.5 9.4,13.5h468.8c4.2,0.5 12.5-4.2 9.4-13.5zm-246.9-455.3c51,1.06581e-14 91.7,43.7 91.7,96.9 0,56.5-79.2,182.3-91.7,203.1-31.3-53.1-91.7-161.5-91.7-203.1 0-53.1 40.6-96.9 91.7-96.9zm-216.7,448l91.7-259.4h41.7c29.9,64.1 83.3,151 83.3,151s81.4-145.7 83.8-151h47.4l91.7,259.4h-439.6z">
+                                    </path>
+                                    <rect width="136.5" x="177.054" y="379.1" height="20.8"></rect>
+                                    <path
+                                        d="m289.554,108.2c0-26-21.9-47.9-47.9-47.9s-47.9,21.9-47.9,47.9 20.8,47.9 47.9,47.9c27.1,0 47.9-21.8 47.9-47.9zm-75-1c0-14.6 11.5-27.1 27.1-27.1s27.1,12.5 27.1,27.1-11.5,27.1-27.1,27.1c-14.6,2.84217e-14-27.1-12.5-27.1-27.1z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>
+                        <span class="text-sm">House #14, Street #12, Darulaman Road, Kabul, Afghanistan.</span>
+                    </div>
+                 
+        
+                </div>
+                <form class="md:col-span-8 p-10">
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-first-name">
+                               Store name
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="grid-first-name" type="text" placeholder="Jane">
+                            {{-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> --}}
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-last-name">
+                               phone number
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-last-name" type="text" placeholder="Doe">
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-first-name">
+                               adress
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                id="grid-first-name" type="text" placeholder="Jane">
+                            {{-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> --}}
+                        </div>
+                        <div class="w-full md:w-1/2 px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-last-name">
+                                city
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-last-name" type="text" placeholder="Doe">
+                        </div>
+                    </div>
+
+
+
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-password">
+                                Email Address
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-email" type="email" placeholder="********@*****.**">
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-password">
+                                password
+                            </label>
+                            <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-email" type="password" placeholder="********">
+                        </div>
+                    </div>
+        
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-full px-3">
+                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-password">
+                              description 
+                            </label>
+                            <textarea rows="10"
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
+                        </div>
+                        <div class="flex justify-between w-full px-3">
+                            <div class="md:flex md:items-center">
+                                <label class="block text-gray-500 font-bold">
+                                    <input class="mr-2 leading-tight" type="checkbox">
+                                    <span class="text-sm">
+                                        Send me your newsletter!
+                                    </span>
+                                </label>
+                            </div>
+                            <button
+                                class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                                type="submit">
+                                Send Message
+                            </button>
+                        </div>
+        
+                    </div>
+        
+                </form>
+        
+            </div>
+        </div>
