6e6d45e6f68e7dc012c6f2d17444b04ad129b1a9
sanaa <sanaaennaji93@gmail.com>
2024-04-08 10:23:25 +0000

    update dtore profile

--- resources/views/stores/profile.blade.php
+++ resources/views/stores/profile.blade.php
@@ -13,7 +13,8 @@
         <div class="border-1  sm:h-[45vh] sm:w-[80%] mx-auto" style="background-image: url(../img/pexels-min-an-694740.jpg)">
             <div class="flex rounded-t-lg bg-top-color sm:px-2 w-full">
                 <div class="overflow-hidden rounded-full sm:relative sm:p-0 sm:top-60 top-35 h-32 w-32 sm:w-25 sm:h-25 ">
-                    <img class="object-cover object-center h-32 sm:h-25" src="../img/téléchargement.jpg"/>
+                    {{-- <img class="object-cover object-center h-32 sm:h-25" src="../img/téléchargement.jpg"/> --}}
+                    <img class="object-cover object-center h-32" src='../img/pexels-tuur-tisseghem-626986.jpg'>
                 </div>
             </div>
             </div>
