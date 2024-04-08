d3f8b7dff6b8ea2e8a3789d3d098856b204aa427
sanaa <sanaaennaji93@gmail.com>
2024-04-08 10:14:35 +0000

    update profile

--- resources/views/stores/profile.blade.php
+++ resources/views/stores/profile.blade.php
@@ -13,10 +13,10 @@
         <div class="border-1  sm:h-[45vh] sm:w-[80%] mx-auto" style="background-image: url(../img/pexels-min-an-694740.jpg)">
             <div class="flex rounded-t-lg bg-top-color sm:px-2 w-full">
                 <div class="overflow-hidden rounded-full sm:relative sm:p-0 sm:top-60 top-35 h-32 w-32 sm:w-25 sm:h-25 ">
-                    <img class="object-cover object-center h-32 sm:h-25" src="../img/bookstore.jpg"/>
+                    <img class="object-cover object-center h-32 sm:h-25" src="../img/pexels-tuur-tisseghem-626986.jpg"/>
                 </div>
             </div>
             </div>
             <div class="border-1 sm:h-[45vh] sm:w-[80%] mx-auto">
-              <h2 class="font-semibold relative">book storte</h2>
+              {{-- <h2 class="font-semibold relative">book storte</h2> --}}
               </div>
