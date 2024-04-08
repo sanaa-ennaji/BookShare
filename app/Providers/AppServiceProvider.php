f5ce9fe3c69ac2001dacdd41d0eb3d26d9e2f91b
sanaa <sanaaennaji93@gmail.com>
2024-04-08 07:28:01 +0000

    binding in the provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
+use App\Services\StoreService;
 use App\Services\CostumerService;
 use App\Repositories\BookRepository;
+use App\Repositories\StoreRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\CategoryRepository;
 use App\Repositories\CostumerRepository;
 use App\Repositories\BookRepositoryInterface;
 use App\Repositories\CategoryRepositoryInterface;
+use App\RepositoryInterfaces\StoreRepositoryInterface;
 use App\RepositoryInterfaces\CostumerRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(CostumerRepositoryInterface::class, CostumerRepository::class);
         $this->app->bind(CostumerService::class , function ($app) {
             return new CostumerService($app->make(CostumerRepositoryInterface::class));
+        });
+        $this->app->bind(StoreRepositoryInterface::class, StoreRepository::class);
+        $this->app->bind(StoreService::class , function ($app) {
+            return new CostumerService($app->make(StoreRepositoryInterface::class));
         });
         $this->app->bind(BookRepositoryInterface::class, BookRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
