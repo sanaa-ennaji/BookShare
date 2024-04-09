9d5135f45fbe23f6f870d82f316d6641beec1d99
sanaa <sanaaennaji93@gmail.com>
2024-04-09 23:36:46 +0000

    update nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav
-  class="flex-no-wrap relative bg-purple-800 flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4">
+  class="flex-no-wrap relative  flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4">
   <div class="flex w-full flex-wrap items-center justify-between px-3">
    
     <div
