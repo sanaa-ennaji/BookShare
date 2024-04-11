d3422317b546006131f3939f392ac46cba539496
sanaa <sanaaennaji93@gmail.com>
2024-04-11 14:51:48 +0000

    update

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -11,7 +11,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,6 +22,8 @@ class CategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
+             
+         'title' => ['required'],
             
         ];
     }
