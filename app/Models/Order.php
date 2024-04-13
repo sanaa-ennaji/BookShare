5b53ce39fd94a964584c63210e34de4444a03cb6
sanaa <sanaaennaji93@gmail.com>
2024-04-13 18:48:20 +0000

    add orderItems class

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'total_price',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function details()
+    {
+        return $this->hasOne(OrderDetail::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
