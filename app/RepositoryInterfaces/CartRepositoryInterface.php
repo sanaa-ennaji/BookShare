9a7d03e759d6dd8c5ed62695dd03cfbe808d76db
sanaa <sanaaennaji93@gmail.com>
2024-04-13 22:05:00 +0000

    update cart

--- app/RepositoryInterfaces/CartRepositoryInterface.php
+++ app/RepositoryInterfaces/CartRepositoryInterface.php
@@ -6,4 +6,6 @@ interface CartRepositoryInterface
 {
     public function addItemToCart($userId, $bookId, $quantity);
     public function getUserCart($userId);
+    public function getTotalPrice($userId);
+    public function createOrder($userId, $totalPrice);
 }
--- app/ServiceInterface/CartServiceInterface.php
+++ app/ServiceInterface/CartServiceInterface.php
@@ -7,4 +7,5 @@ interface CartServiceInterface
     public function addToCart($userId, $bookId, $quantity);
     public function getUserCart($userId);
     public function calculateTotalPrice($cartItems);
+    public function createOrder($userId);
 }
