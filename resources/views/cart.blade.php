7192b240205d25ace69a5f81af505b0e6083c4c9
sanaa <sanaaennaji93@gmail.com>
2024-04-19 12:25:50 +0100

    update cart ui

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -95,25 +95,33 @@
 
         </div>
 
-        <!-- Sub total -->
-        <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-          <div class="mb-2 flex justify-between">
+        
+        <form  id="create-order-form" action="{{ route('orders.create') }}" method="POST" class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+          @csrf       
+             <div class="mb-2 flex justify-between">
             <p class="text-gray-700">Subtotal</p>
             <p class="text-gray-700">$129.99</p>
           </div>
           <div class="flex justify-between">
-            {{-- <p class="text-gray-700">Shipping</p> --}}
-            {{-- <p class="text-gray-700">$4.99</p> --}}
+            <p class="text-gray-700">Shipping</p> 
+          <p class="text-gray-700">$4.99</p>
           </div>
+          
           <hr class="my-4" />
+         
+            
+      
+      
+       
           <div class="flex justify-between">
+        
             <p class="text-lg font-bold">Total</p>
             <div class="">
               <p class="mb-1 text-lg font-bold">${{$totalPrice}} MAD</p>
-              {{-- <p class="text-sm text-gray-700">including </p> --}}
+              <input type="hidden" name="total_price" value="${{$totalPrice}}">
             </div>
           </div>
-          <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
+          <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600" type="submit">Check out</button>
         </div>
       </div>
     </div>
