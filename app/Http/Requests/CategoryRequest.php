841b26345f4b61eaca48769f194551c5ad5db52a
sanaa <sanaaennaji93@gmail.com>
2024-04-21 14:36:12 +0100

    update category

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
         return [
              
          'title' => ['required'],
-         'image' => ['required']
+        //  'image' => ['required'],
             
         ];
 
--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -46,7 +46,7 @@
                         <input type="text" id="title" name="title"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         </div> 
-                        <div>
+                        {{-- <div>
 
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 for="file_input">Upload file</label>
@@ -56,7 +56,7 @@
                                  type="file">
                             <p class="mt-1 text-sm text-blue-500 dark:text-gray-300" id="file_input_help">SVG, PNG,
                                 JPG or GIF (MAX. 800x400px).</p>
-                        </div>
+                        </div> --}}
                     </div>
                     <button type="submit" 
                         class="left-0 text-white inline-flex items-center bg-blue-600  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
