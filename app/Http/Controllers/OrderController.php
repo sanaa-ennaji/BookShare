7a472b421dcf909305b8966acf5f0a10ef6f7021
sanaa <sanaaennaji93@gmail.com>
2024-04-23 11:25:48 +0100

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -28,30 +28,28 @@ class OrderController extends Controller
               
                 $customerId = $user->costumer->id;
                
-                // dd($customerId);
+               
                 $order = $this->orderService->createOrder($customerId);
         
-                // Calculate the total price of the order
+              
                 $totalPrice = $this->orderService->calculateTotalPrice($customerId);
         
-                // Charge the customer using Stripe
+              
                 $charge = \Stripe\Charge::create([
-                    'amount' => $totalPrice * 100, // Stripe requires amount in cents
-                    'currency' => 'usd', // Change this to your currency
+                    'amount' => $totalPrice * 100, 
+                    'currency' => 'usd', 
                     'source' => $request->stripeToken,
                     'description' => 'Payment for Order #' . $order->id,
                     'costumer' => $customerId,
                 ]);
         
-                // Check if the charge was successful
+                
                 if ($charge->status === 'succeeded') {
-                    // Payment was successful, update order status to 'Paid'
                     $order->status = 'Paid';
                     $order->save();
         
                     return response()->json(['message' => 'Order created and paid successfully'], 200);
                 } else {
-                    // Payment failed, rollback order creation and return error message
                     DB::rollBack();
                     return response()->json(['error' => 'Payment failed'], 400);
                 }
