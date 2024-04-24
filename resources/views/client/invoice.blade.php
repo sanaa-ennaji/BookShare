45c93a974da5fff66b3a2dd68b66acff7a7edf6a
sanaa <sanaaennaji93@gmail.com>
2024-04-24 17:21:10 +0100

    update invoice

--- resources/views/client/invoice.blade.php
+++ resources/views/client/invoice.blade.php
@@ -1,4 +1,124 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Invoice</title>
+    <style>
+      body{
+        margin: 2rem  10rem;
+      }
+      h4 {
+    margin: 0;
+}
+.w-full {
+    width: 100%;
+}
+.w-half {
+    width: 50%;
+}
+.margin-top {
+    margin-top: 1.25rem;
+}
+.footer {
+    font-size: 0.875rem;
+    padding: 1rem;
+    background-color: rgb(241 245 249);
+}
+table {
+    width: 100%;
+    border-spacing: 0;
+}
+table.products {
+    font-size: 0.875rem;
+}
+table.products tr {
+    background-color: rgb(96 165 250);
+}
+table.products th {
+    color: #ffffff;
+    padding: 0.5rem;
+}
+table tr.items {
+    background-color: rgb(241 245 249);
+}
+table tr.items td {
+    padding: 0.5rem;
+}
+.total {
+    text-align: right;
+    margin-top: 1rem;
+    font-size: 0.875rem;
+}
+    </style>
+</head>
+<body>
+    <table class="w-full">
+        <tr>
+            <td class="w-half">
+                <img src="{{ asset('laraveldaily.png') }}" alt="laravel daily" width="200" />
+            </td>
+            <td class="w-half">
+                <h2>Invoice ID: 834847473</h2>
+            </td>
+        </tr>
+    </table>
+ 
+    <div class="margin-top">
+        <table class="w-full">
+            <tr>
+                <td class="w-half">
+                    <div><h4>To:</h4></div>
+                    <div>John Doe</div>
+                    <div>123 Acme Str.</div>
+                </td>
+                <td class="w-half">
+                    <div><h4>From:</h4></div>
+                    <div>Laravel Daily</div>
+                    <div>London</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+ 
+    <div class="margin-top">
+        <table class="products">
+            <tr>
+                <th>Qty</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+            <tr class="items">
+              
+                    <td>
+                     ziuerh
+                    </td>
+                    <td>
+                       zebziuefuiezf
+                    </td>
+                    <td>
+                        300$
+                    </td>
+               
+            </tr>
+        </table>
+    </div>
+ 
+    <div class="total">
+        Total: $129.00 USD
+    </div>
+ 
+    <div class="footer margin-top">
+        <div>Thank you</div>
+        <div>&copy; Laravel Daily</div>
+    </div>
+</body>
+</html>
+
+
+
+{{-- <!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="utf-8">
@@ -95,10 +215,10 @@
             <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
           </div>
         </main>
-        
+
         <footer>
           Invoice was created on a computer and is valid without the signature and seal.
         </footer>
    
 </body>
-</html>  
+</html>   --}}
