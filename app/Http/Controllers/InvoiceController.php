cdc9339bd40918899975ced831b8332a1e9d562e
sanaa <sanaaennaji93@gmail.com>
2024-04-25 12:07:15 +0100

    add order details in invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Barryvdh\DomPDF\PDF;
+use App\Models\OrderItem;
+use App\Models\Orderline;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
@@ -13,4 +16,31 @@ class InvoiceController extends Controller
 
     //     return $pdf->download('itcodestuff.pdf');
     // }
+
+
+
+
+public function displayOrderItems()
+{
+    try {
+      
+        $orderId = session('order_id');
+
+        if (!$orderId) {
+            throw new \Exception("Order ID not found in session");
+        }
+
+        $order = Order::findOrFail($orderId);
+
+      
+        $orderItems = Orderline::where('order_id', $orderId)->get();
+
+       
+        return view('invoice', ['order' => $order, 'orderItems' => $orderItems]);
+    } catch (\Exception $e) {
+    
+        return response()->json(['error' => $e->getMessage()], 400);
+    }
+}
+
 }
