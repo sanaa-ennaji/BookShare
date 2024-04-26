d5c2c58b794cb59530f7521b9aafd3da999f65bc
sanaa <sanaaennaji93@gmail.com>
2024-04-26 11:24:11 +0100

    add

--- app/Http/Controllers/ratingController.php
+++ app/Http/Controllers/ratingController.php
@@ -8,9 +8,4 @@ use Barryvdh\DomPDF\Facade\Pdf;
 class ratingController extends Controller
 {
  
-    public function download() {
-        $pdf = Pdf::loadView('pdf');
-     
-        return $pdf->download();
-    }
 }
--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,4 +1,14 @@
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- @vite('resources/css/app.css') --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+   
+  </head>
+
+{{-- <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -12,4 +22,4 @@
     <title>Admin</title>
 
    
-</head>
+</head> --}}
