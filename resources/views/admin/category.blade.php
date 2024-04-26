3d902aa63982bab0935c6b9aaded2d9e07545cde
sanaa <sanaaennaji93@gmail.com>
2024-04-26 15:02:42 +0100

    update admin

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -5,90 +5,62 @@
   
 
     @include('components/sidebare')
-    <div class="text-gray-800 font-inter fixed top-0 right-0 w-[90%]">
+   
+
+
 
     <div class="mt-20 ml-40">
         <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" id="btn"
-        class="relative block text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="relative block text-white bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-40 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button">
         add new Category
     </button>
+    
 
 
-    {{-- <div id="deleteModal" class="hidden fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex justify-center items-center">
-        <div class="modal-content bg-white p-6 rounded-lg shadow-lg w-[50%] h-[25%] mx-auto mt-20">
-            <h2 class="text-xl font-semibold mb-4">Delete Category</h2>
-            <p class="mb-4">Are you sure you want to delete this category?</p>
-            <div class="flex justify-end">
-                <form id="deleteForm" action="/delete" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2">Delete</button>
-                    <button type="button" onclick="closeModal()" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">Cancel</button>
-                </form>
-            </div>
+
+    <div id="crud-modal"
+    class="hidden overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex justify-center items-center">
+    <div class="relative p-4 w-full max-w-[30%] max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                Create Category
+            </h3>
+            <button type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-toggle="crud-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close</span>
+            </button>
         </div>
-    </div> --}}
-
-    <div id="crud-modal" class="hidden fixed inset-0 z-50 overflow-y-auto overflow-x-hidden flex justify-center items-center">
-    <div class="relative p-4 w-full max-w-[30%] max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Create Category
-                </h3>
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="crud-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                        <span class="sr-only">Close</span>
-                    </button>
+        <form class="p-4 md:p-5" method="POST" action="/createCategory" enctype="multipart/form-data">
+            @csrf
+            <div class="grid gap-4 mb-4 grid-cols-1">
+                <div class="col-span-2 sm:col-span-1">
+                    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                    <input type="text" id="title" name="title"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                 </div>
-
-              
-
-                <form class="p-4 md:p-5" method="POST" action="/createCategory"  enctype="multipart/form-data">
-                    @csrf
-                    <div class="grid gap-4 mb-4 grid-cols-1">
-                        <div class="col-span-2 sm:col-span-1">
-                            <label for="title"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">title</label>
-                        <input type="text" id="title" name="title"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                        </div> 
-                        {{-- <div>
-
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                for="file_input">Upload file</label>
-                            <input
-                                class="block w-full text-sm text-blue-900 border border-blue-300 rounded-lg cursor-pointer bg-blue-50 dark:text-blue-400 focus:outline-none dark:bg-blue-700 dark:border-blue-600 dark:placeholder-blue-400"
-                                aria-describedby="file_input_help" name="image" id="images" 
-                                 type="file">
-                            <p class="mt-1 text-sm text-blue-500 dark:text-gray-300" id="file_input_help">SVG, PNG,
-                                JPG or GIF (MAX. 800x400px).</p>
-                        </div> --}}
-                    </div>
-                    <button type="submit" 
-                        class="left-0 text-white inline-flex items-center bg-blue-600  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        confirm
-                    </button>
-                </form>
             </div>
-        </div>
+            <button type="submit"
+                class="left-0 text-white inline-flex items-center bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                        clip-rule="evenodd"></path>
+                </svg>
+                Confirm
+            </button>
+        </form>
     </div>
+</div>
 
-    </div>
-
+<div class="text-gray-800 font-inter fixed top-25 right-0 w-[90%]">
 <div class="mt-10 bg-gray-50 min-h-screen w-[80%] mx-auto">
       <div class=" p-4 rounded-md">
         <div>
