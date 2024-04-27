117aecc07f9bb88a40b692d63d2ca382f9c33c69
sanaa <sanaaennaji93@gmail.com>
2024-04-28 00:12:50 +0100

    update

--- resources/views/admin/costumers.blade.php
+++ resources/views/admin/costumers.blade.php
@@ -65,6 +65,7 @@
                                 <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
                                     <div class="flex items-center gap-x-2">
                                         <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
+                                      
                                         <div>
                                             <h2 class="text-sm font-medium text-gray-800 dark:text-white ">Arthur Melo</h2>
                                             <p class="text-xs font-normal text-gray-600 dark:text-gray-400">authurmelo@example.com</p>
--- resources/views/stores/updateProfile.blade.php
+++ resources/views/stores/updateProfile.blade.php
@@ -25,7 +25,7 @@
                         <div class="container mx-auto bg-white dark:bg-gray-800 rounded">
                             <div class="xl:w-full border-b border-gray-300 dark:border-gray-700 py-5 bg-white dark:bg-gray-800">
                                 <div class="flex w-11/12 mx-auto xl:w-full xl:mx-0 items-center">
-                                    <p class="text-lg text-gray-800 dark:text-gray-100 font-bold">Profile</p>
+                                    <p class="text-lg text-gray-800 dark:text-gray-100 font-bold">Store Profile</p>
                                     <div class="ml-2 cursor-pointer text-gray-600 dark:text-gray-400">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
                                             <path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="currentColor" />
@@ -36,37 +36,40 @@
                             <div class="mx-auto">
                                 <div class=" mx-auto xl:mx-0">
                                     <div class="rounded relative mt-8 h-48">
-                                        <img src="https://cdn.tuk.dev/assets/webapp/forms/form_layouts/form1.jpg" alt="" class="w-full h-full object-cover rounded absolute shadow" />
+                                        <img id='preview_img' src="" alt="" class="w-full h-full object-cover rounded absolute shadow" />
                                         <div class="absolute bg-black opacity-50 top-0 right-0 bottom-0 left-0 rounded"></div>
-                                        <div class="flex items-center px-3 py-2 rounded absolute right-0 mr-4 mt-4 cursor-pointer">
-                                            <p class="text-xs text-gray-100">Change Cover Photo</p>
-                                            <div class="ml-2 text-gray-100">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                                    <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                                                    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                                                    <line x1="16" y1="5" x2="19" y2="8" />
-                                                </svg>
-                                            </div>
+                                        <div class="flex items-center px-3 py-2 rounded absolute right-0  mt-4 cursor-pointer">
+                                           
+                                            <input name="picture" type="file" onchange="loadFile(event)" class="block w-full text-sm text-slate-500 
+                                            file:-mr-50 file:py-2 file:px-4
+                                            file:rounded-full file:border-0
+                                            file:text-sm file:font-semibold
+                                            file:text-blue-700
+                                            hover:file:bg-blue-100
+                                         
+                                        " />
+                                        
                                         </div>
                                         <div class="w-20 h-20 rounded-full bg-cover bg-center bg-no-repeat absolute bottom-0 -mb-10 ml-12 shadow flex items-center justify-center">
-                                            <img src="https://cdn.tuk.dev/assets/webapp/forms/form_layouts/form2.jpg" alt="" class="absolute z-0 h-full w-full object-cover rounded-full shadow top-0 left-0 bottom-0 right-0" />
-                                            <div class="absolute bg-black opacity-50 top-0 right-0 bottom-0 left-0 rounded-full z-0"></div>
-                                            <div class="cursor-pointer flex flex-col justify-center items-center z-10 text-gray-100">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" />
-                                                    <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                                                    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                                                    <line x1="16" y1="5" x2="19" y2="8" />
-                                                </svg>
-                                                <p class="text-xs text-gray-100">Edit Picture</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-16 flex flex-col xl:w-2/6 lg:w-1/2 md:w-1/2 w-full">
-                                        <label for="username" class="pb-2 text-sm font-bold text-gray-800 dark:text-gray-100">Username</label>
-                                        <input tabindex="0" type="text" id="username" name="username" required class="border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-blue-700 bg-transparent placeholder-gray-500 text-gray-600 dark:text-gray-400" placeholder="@example" />
-                                    </div>
+                                         <div class="flex items-center justify-center space-x-6 my-5  rounded-md">
+            <label class="block">
+                <span class="sr-only">Choose profile photo</span>
+                <input name="picture" type="file" onchange="loadFile2(event)" class="block w-full text-sm text-slate-500 
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-full file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-violet-50 file:text-violet-700
+                    hover:file:bg-violet-100
+                    hidden
+                " />
+                <img id='preview_img2' class="h-20 w-20 object-cover rounded-full shadow-lg cursor-pointer"
+                    src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais"
+                    alt="Current profile photo" />
+            </label>
+        </div>
+    </div>
+            </div>
+                                   
                                     <div class="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
                                         <label for="about" class="pb-2 text-sm font-bold text-gray-800 dark:text-gray-100">About</label>
                                         <textarea id="about" name="about" required class="bg-transparent border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-blue-700 resize-none placeholder-gray-500 text-gray-600 dark:text-gray-400" placeholder="Let the world know who you are" rows="5"></textarea>
@@ -104,32 +107,43 @@
             </div>
         </div>
               
+    
+
                 <script>
-                    let form = document.getElementById("login");
-                    form.addEventListener(
-                        "submit",
-                        function (event) {
-                            event.preventDefault();
-                            let elements = form.elements;
-                            let payload = {};
-                            for (let i = 0; i < elements.length; i++) {
-                                let item = elements.item(i);
-                                switch (item.type) {
-                                    case "checkbox":
-                                        payload[item.name] = item.checked;
-                                        break;
-                                    case "submit":
-                                        break;
-                                    default:
-                                        payload[item.name] = item.value;
-                                        break;
-                                }
-                            }
-                         
-                            console.log("payload", payload);
-                        },
-                        true
-                    );
+                  
+
+
+
+                    var loadFile = function(event) {
+
+var input = event.target;
+var file = input.files[0];
+var type = file.type;
+
+var output = document.getElementById('preview_img');
+
+
+output.src = URL.createObjectURL(event.target.files[0]);
+output.onload = function() {
+    URL.revokeObjectURL(output.src) 
+}
+};
+
+
+var loadFile2 = function(event) {
+
+var input = event.target;
+var file = input.files[0];
+var type = file.type;
+
+var output = document.getElementById('preview_img2');
+
+
+output.src = URL.createObjectURL(event.target.files[0]);
+output.onload = function() {
+    URL.revokeObjectURL(output.src) 
+}
+};
                 </script>
              
 </body>
