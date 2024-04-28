7511851d4fa50ffa01629c1822c27917258f1feb
sanaa <sanaaennaji93@gmail.com>
2024-04-28 22:39:42 +0100

    updations

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Stripe\Customer;
 use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
+use Laravel\Socialite\Facades\Socialite;
 
 class GoogleLoginController extends Controller
 {
@@ -20,13 +21,27 @@ class GoogleLoginController extends Controller
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
         $user = User::where('email', $googleUser->email)->first();
-        if(!$user)
-        {
-            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => \Hash::make(rand(100000,999999))]);
+    
+        if (!$user) {
+            $user = User::create([
+                'name' => $googleUser->name,
+                'email' => $googleUser->email,
+                'password' => \Hash::make(rand(100000,999999))
+            ]);
         }
-
+    
+    
+        if (!$user->customer) {
+           
+            $customer = Customer::create([
+                'user_id' => $user->id,
+             
+            ]);
+        }
+    
         Auth::login($user);
-
+    
         return redirect()->back();
     }
+    
 }
