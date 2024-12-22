7bbb4c11010a5bec46872fe8a9f5b328cf48c48c
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-22 14:15:50 +0100

    update readme

---
+++ IdeaProjects/demo (1)/YouQuiz/src/main/java/org/sanaa/setnence/citronix/youquiz/YouQuizApplication.java
@@ -0,0 +1,13 @@
+package org.sanaa.setnence.citronix.youquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouQuizApplication.class, args);
+    }
+
+}
